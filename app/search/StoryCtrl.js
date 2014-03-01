89d9272b91d1a3bbd0d433a7557634856ad0217c
Barton <bartonhammond@gmail.com>
2014-03-01 13:30:32 -0600

    Preview image

--- app/search/StoryCtrl.js
+++ app/search/StoryCtrl.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('fv')
-  .controller('StoryCtrl', function ($scope) {
+  .controller('StoryCtrl', function ($scope, fileReader) {
+    
 
     $scope.init = function(aUser) {
       console.log('storyCtrl: init');
       console.log(aUser);
       $scope.user = aUser;
     };
+    $scope.setFiles = function(element) {
+      $scope.photo = element.files[0];
+      $scope.photoFile = element.files[0];
+    };
+    
+    $scope.getFile = function () {
+      $scope.progress = 0;
+      fileReader.readAsDataUrl($scope.file, $scope)
+        .then(function(result) {
+          $scope.imageSrc = result;
+        });
+    };
+
   });
+
